34127ac11e40f0f641924c24c35e6be958aa0dd1
PeterGuek <76177881+PeterGuek@users.noreply.github.com>
2024-07-19 20:19:46 -0700

    Create main.py

---
+++ Exercice2/main.py
@@ -0,0 +1,17 @@
+students = {
+    'Alice': {
+         'Mathematiques': 90,
+         'Francais': 80,
+         'Histoire': 95
+    },
+    'Bob': {
+         'Mathematiques': 75,
+         'Francais': 85,
+         'Histoire': 70
+    },
+     'Charlie': {
+         'Mathematiques': 88,
+         'Francais': 92,
+         'Histoire': 78
+     }
+}
