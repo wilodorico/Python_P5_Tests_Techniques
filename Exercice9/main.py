63537fad173dbfaa2f15655317d228692f463008
PeterGuek <76177881+PeterGuek@users.noreply.github.com>
2024-07-19 20:50:52 -0700

    Create main.py

---
+++ Exercice9/main.py
@@ -0,0 +1,6 @@
+# Test de la classe Rectangle
+rectangle = Rectangle(5, 3) # 5:width & 3:length
+print("Largeur:", rectangle.width)
+print("Longueur:", rectangle.length)
+print("Aire:", rectangle.calculate_area())
+print("Périmètre:", rectangle.calculate_perimeter())
