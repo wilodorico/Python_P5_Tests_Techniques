721621aec688d260073a55ca2f227783b4f6eb02
wilfried <wilfried.odorico@gmail.com>
2024-09-27 08:44:55 +0200

    Exercise 9 completed

--- Exercice09/main.py
+++ Exercice09/main.py
@@ -1,5 +1,17 @@
 # Test de la classe Rectangle
-rectangle = Rectangle(5, 3) # 5:width & 3:length
+class Rectangle:
+    def __init__(self, width: int, length: int):
+        self.width = width
+        self.length = length
+
+    def calculate_area(self):
+        return self.width * self.length
+
+    def calculate_perimeter(self):
+        return 2 * (self.width + self.length)
+
+
+rectangle = Rectangle(5, 3)  # 5:width & 3:length
 print("Largeur:", rectangle.width)
 print("Longueur:", rectangle.length)
 print("Aire:", rectangle.calculate_area())
