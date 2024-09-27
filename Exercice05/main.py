f6a7e2c5a14b4199b4c38cf53df5b80b5ba93f2d
wilfried <wilfried.odorico@gmail.com>
2024-09-27 08:44:21 +0200

    Exercise 5 completed

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,24 @@
 def sum(a, b):
+    """Calculate the sum of two numbers.
+
+    Args:
+        a (int, float): The first number to add.
+        b (int, float): The second number to add.
+
+    Returns:
+        (int, float): The sum of the two numbers
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """Calculate the difference between two numbers.
+
+    Args:
+        a (int, float): The number from which to sub.
+        b (int, float): The number to subtract.
+
+    Returns:
+        (int, float): The difference between the two numbers.
+    """
     return a - b
