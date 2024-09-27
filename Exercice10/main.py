ff9f4c042cc1469b1aa79913144eba70695de245
wilfried <wilfried.odorico@gmail.com>
2024-09-27 08:45:02 +0200

    Exercise 10 completed

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,23 @@
-## Écrivez votre code ici !
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        print(f"Mon nom est {self.name} et j'ai {self.age} ans.")
+
+
+class Employee(Person):
+    def __init__(self, name: str, age: int, salary: int):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def display_details(self):
+        print(f"Mon nom est {self.name}, j'ai {self.age} ans. Mon salaire est de {self.salary}€")
+
+
+person = Person("Wilfried", 44)
+person.display_details()
+
+employee = Employee("Jack", 46, 3500)
+employee.display_details()
