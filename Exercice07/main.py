1e9782b129452bba42fd6a122cb806b250bdd6be
wilfried <wilfried.odorico@gmail.com>
2024-09-27 08:44:38 +0200

    Exercise 7 completed

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,19 @@
-## Écrivez votre code ici !
+# Écrivez votre code ici !
+def square(number: int | float) -> int | float:
+    """Square a number
+
+    Args:
+        number (int | float): The number to square
+
+    Returns:
+        int | float: The square of the number
+    Raises:
+        TypeError: If the number is not an int or float
+    """
+    if not isinstance(number, (int, float)):
+        raise TypeError(f"Error: {type(number)} is not an int or float")
+    return number * number
+
+
+s = square("8")
+print(s)
