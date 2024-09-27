979a260795ee3a95d5893b83de210ef70a7a1328
wilfried <wilfried.odorico@gmail.com>
2024-09-27 08:44:46 +0200

    Exercise 8 completed

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,20 @@
 def log_decorator(func):
-     pass
- 
+    def wrapper(*args, **kwargs):
+        print("====================")
+        print("Avant la fonction !")
+        print("====================")
+        result = func(*args, **kwargs)
+        print("====================")
+        print("Après la fonction :)")
+        print("====================")
+        return result
+
+    return wrapper
+
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
+
 
 function_test()
